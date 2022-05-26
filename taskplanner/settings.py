bd8170e699fd145dc6c41f4ac2f78ef912fd1102
Sanjay <sanjaybana50@gmail.com>
2022-05-26 14:48:50 +0530

    update_requirements and gunicorn

--- taskplanner/settings.py
+++ taskplanner/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['./static/']
 
@@ -137,3 +138,4 @@ LOGIN_REDIRECT_URL = "Todolist"
 LOGIN_URL = "login"
 
 django_heroku.settings(locals())
+
